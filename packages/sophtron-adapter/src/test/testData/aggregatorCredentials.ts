d5dbe7036f0c994e9a0bd1cb965d548e390b308c
Tyson Phalp <tyson.phalp@mx.com>
2024-11-25 15:03:58 -0700

    Remove unused params

--- packages/sophtron-adapter/src/test/testData/aggregatorCredentials.ts
+++ packages/sophtron-adapter/src/test/testData/aggregatorCredentials.ts
@@ -1,4 +1,3 @@
-import { SOPHTRON_ADAPTER_NAME } from "../../constants";
 import type { ApiCredentials } from "../../models";
 
 export const aggregatorCredentials: ApiCredentials = {
@@ -6,6 +5,4 @@ export const aggregatorCredentials: ApiCredentials = {
   secret: "testSecret",
   endpoint: "https://api.sophtron.com/api",
   vcEndpoint: "https://vc.sophtron.com/api/",
-  aggregator: SOPHTRON_ADAPTER_NAME,
-  available: true,
 };
